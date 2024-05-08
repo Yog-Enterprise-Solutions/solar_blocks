d1b00a139beef18e02a4615baccb82e730d25f54
salonipatell <01saloniradhvi@gmail.com>
2024-05-08 04:27:55 -0900

    lead report

--- solar_blocks/solar_blocks/report/lead/lead.py
+++ solar_blocks/solar_blocks/report/lead/lead.py
@@ -8,7 +8,8 @@ from frappe.utils import now_datetime, get_datetime
 
 def execute(filters=None):
 	columns, data = get_columns(), get_data(filters)
-	frappe.throw(f"{data}")
+	# frappe.throw(f"{data}")
+	# frappe.log_error('data',data)
 	if not data:
 		frappe.msgprint("No records found")
 	return columns, data
@@ -20,12 +21,12 @@ def execute(filters=None):
 def get_columns():
 	columns = [
 	
-		{"fieldname": "first_name", "label": ("Name of job"), "fieldtype": "Data", "width": 200},
+		{"fieldname": "first_name", "label": ("Name of job"), "fieldtype": "Data", "width": 150},
 		{"fieldname": "source", "label": ("Source"), "fieldtype": "Data", "width": 200},
 		{"fieldname": "creation", "label": ("Date Created"), "fieldtype": "Data", "width": 200},
 		{"fieldname": "lead_owner", "label": ("Created By"), "fieldtype": "Data", "width": 200},
 		{"fieldname": "aging", "label": ("Ageing"), "fieldtype": "Data", "width": 100},
-		{"fieldname": "custom_customer_availability", "label": ("Additional Notes"), "fieldtype": "Data", "width": 100}
+		{"fieldname": "custom_customer_availability", "label": ("Additional Notes"), "fieldtype": "Data", "width": 200},
 		{"fieldname": "timeline", "label": ("Timeline"), "fieldtype": "Data", "width": 100}
 		
 	]
@@ -40,24 +41,30 @@ def get_data(filters):
 		doctype="Lead",
 		fields=["first_name","lead_owner","creation","source","custom_customer_availability","lead_sub_status"])
 	
-
+	total_age=0
+	total_no_of_jobs=0
 	for lead in data:
 		creation_date = lead.get('creation')
 		if creation_date:
 			days_aging = (datetime.now() - creation_date).days
 			lead['aging'] =days_aging
+			total_age+=days_aging
+			total_no_of_jobs+=1
+			lead['creation']=frappe.utils.getdate(creation_date)
 			after_three_day = frappe.utils.add_to_date(frappe.utils.getdate(creation_date), days=3)
-			# if lead.get('lead_sub_status')=='Lead':
-			# 	if frappe.utils.getdate()>after_three_day:
-			# 		lead['timeline']=Late
-			# elif lead.get('lead_sub_status')!='Lead':
-			# 	if after_three_day==frappe.utils.getdate():
-			# 		lead['timeline']=On timeline
-			# 	elif frappe.utils.getdate()<after_three_day:
-			# 		lead['timeline']=Before time
-
-
-		
+			if lead.get('lead_sub_status')=='Lead':
+				if frappe.utils.getdate()>after_three_day:
+					lead['timeline']='Late'
+			elif lead.get('lead_sub_status')!='Lead':
+				if after_three_day==frappe.utils.getdate():
+					lead['timeline']='On time'
+				if frappe.utils.getdate()<after_three_day:
+					lead['timeline']='Before time'
+				if frappe.utils.getdate()>after_three_day:
+					lead['timeline']='Late'
+	# frappe.throw(f"{total_no_of_jobs}"
+	average=round((total_age/total_no_of_jobs),2)
+	data.append({'first_name': '', 'lead_owner': 'Average Ageing', 'creation':'', 'source': '', 'custom_customer_availability':'', 'lead_sub_status': '', 'aging':average, 'timeline': ''})
 	return data
 
 
